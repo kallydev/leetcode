1f9f53fd54648a8de8e2434487784d5cabc39194
kallydev <kallydev@gmail.com>
2023-01-02 14:29:50 +0800

    submit(algorithm/290): go solution

---
+++ algorithm/290/go/solution_1.go
@@ -0,0 +1,41 @@
+package main
+
+import "strings"
+
+func wordPattern(pattern string, s string) bool {
+	words := strings.Split(s, "\x20")
+
+	if len(pattern) != len(words) {
+		return false
+	}
+
+	var (
+		charToWord = make(map[rune]string)
+		wordToChar = make(map[string]rune)
+	)
+
+	for index, char := range pattern {
+		word := words[index]
+
+		internalChar, exists := wordToChar[word]
+
+		switch {
+		case !exists:
+			wordToChar[word] = char
+		case exists && internalChar != char:
+			return false
+		}
+
+		internalWord, exists := charToWord[char]
+		if !exists {
+			internalWord = word
+			charToWord[char] = internalWord
+		}
+
+		if internalWord != word {
+			return false
+		}
+	}
+
+	return true
+}
