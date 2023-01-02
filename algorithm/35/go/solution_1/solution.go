d274629d5ea2fe2930b8d940d5acd0a399d931fb
kallydev <kallydev@gmail.com>
2023-01-02 18:01:28 +0800

    submit(algorithm/35): go solution

---
+++ algorithm/35/go/solution_1/solution.go
@@ -0,0 +1,21 @@
+package solution
+
+func searchInsert(nums []int, target int) int {
+	left, middle, right := 0, 0, len(nums)-1
+
+	for left <= right {
+		middle = left + (right-left)/2
+		number := nums[middle]
+
+		switch {
+		case number == target:
+			return middle
+		case number > target:
+			right = middle - 1
+		case number < target:
+			left = middle + 1
+		}
+	}
+
+	return left + 1
+}
